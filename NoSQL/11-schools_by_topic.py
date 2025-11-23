572fcb78e009b88a28e31dda8344def5fca35e19
egigj-dev <egigj.dev@gmail.com>
2025-11-23 01:29:56 +0100

    Add exc. 9-12

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python3
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of schools having a specific topic
+    """
+    schools = list(mongo_collection.find({"topics": topic}))
+    return schools
