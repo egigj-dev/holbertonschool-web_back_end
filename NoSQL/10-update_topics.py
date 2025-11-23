c0fa07f19dbeb4f36867a0285283e9d4696591e0
egigj-dev <egigj.dev@gmail.com>
2025-11-23 23:49:26 +0100

    fix: exc. 9-12

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """Script that updates topics of a document based on the name"""
 
 
--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,4 +1,5 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
+"""Returns the list of schools having a specific topic"""
 
 
 def schools_by_topic(mongo_collection, topic):
