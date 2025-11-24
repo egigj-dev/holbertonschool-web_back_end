1c54cbc26ce5862340d5e1a2bf886e613e9b400b
egigj-dev <egigj.dev@gmail.com>
2025-11-24 21:53:58 +0100

    Add: exc. 14

---
+++ NoSQL/101-students.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Top students """
+
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by average score
+    """
+    pipeline = [
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ]
+    
+    return list(mongo_collection.aggregate(pipeline))
