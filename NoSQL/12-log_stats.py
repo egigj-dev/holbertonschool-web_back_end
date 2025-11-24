83c0b32f2d92c3c175425792f25e21f7b902d04e
egigj-dev <egigj.dev@gmail.com>
2025-11-24 20:57:55 +0100

    fix: exc. 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-cript that provides stats about Nginx logs stored in MongoDB
+Script that provides stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
@@ -10,18 +10,18 @@ if __name__ == "__main__":
     nginx_collection = client.logs.nginx
     
     # Total number of logs
-    total_logs = nginx_collection.count()
+    total_logs = nginx_collection.count_documents({})
     print("{} logs".format(total_logs))
     
     # Methods stats
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = nginx_collection.count({"method": method})
+        count = nginx_collection.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, count))
     
     # Status check count
-    status_check = nginx_collection.count({
+    status_check = nginx_collection.count_documents({
         "method": "GET",
         "path": "/status"
     })
